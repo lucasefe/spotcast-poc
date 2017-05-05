d88d777f3a01f391b96dc959c5a8bb59112763da
Lucas Florio <lucasefe@gmail.com>
2017-05-05 13:09:21 -0300

    Copy over client/server websocket implementation

--- server/main.go
+++ server/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+	"net/http"
+)
 
+var addr = flag.String("addr", ":8081", "http service address")
+
+func main() {
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", 404)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
 }
